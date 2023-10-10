52b0eaf9e1142f559a2377182479295d5c3fbd6c
emanuellydutra05 <115026458+emanuellydutra05@users.noreply.github.com>
2023-10-10 08:48:15 -0300

    10/10

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 function tocaSomPomn() {
     document.querySelector('#som_tecla_pom').play();
     {
-      const listaDeTeclas =  document.querySelectorAll('.tecla')
+        const listaDeTeclas = document.querySelectorAll('.tecla')
+
+        let contador = 0;
+
+        while (contador < listaDeTeclas.length) {
+
+            listaDeTeclas[contador].onclick = tocaSomPom;
+
+            contador = contador + 1;
+
+            console.log(contador);
+
+        }
