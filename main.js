44cc5d2559bf682d39d0452fdb50989b31cf45ab
emanuellydutra05 <115026458+emanuellydutra05@users.noreply.github.com>
2023-10-24 08:32:43 -0300

    24/10

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
-function tocaSomPomn() {
-    document.querySelector('#som_tecla_pom').play();
-    {
-        const listaDeTeclas = document.querySelectorAll('.tecla')
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 
-        let contador = 0;
+}
+const listaDeTeclas = document.querySelectorAll('.tecla');
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = '#som_${instrumento}'; //template string
 
-        while (contador < listaDeTeclas.length) {
+    tecla.onclick = function (evento) {
+        tocaSom(idAudio);
 
-            listaDeTeclas[contador].onclick = tocaSomPom;
+    }
 
-            contador = contador + 1;
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'space'// evento.code === 'Enter') {
+                tecla.classList.add('ativa');
 
-            console.log(contador);
-
-        }
+    }
+}
+tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
+}
+    }
